28aa718dcc80b646a6965aed40a2873fc1c8cca4
Dong Hyun <decidere@naver.com>
2024-11-21 20:12:06 +0900

    [Feat]: 식당 상세 조회 컨트롤러 구현

---
+++ controllers/restaurant.controller.js
@@ -0,0 +1,50 @@
+const db = require("../utils/db");
+const jwt = require("jsonwebtoken");
+
+// 식당 상세 조회 컨트롤러
+exports.getRestaurantDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    let sql = `SELECT name, category, address, hours, phone FROM restaurants WHERE id=${id}`;
+    // 이름 라벨 주소 영업시간 전화번호 데이터 조회
+    const [restaurantInfo] = await db.execute(sql);
+    let { name, category, address, hours, phone } = restaurantInfo[0];
+
+    // 메뉴 데이터 조회
+    sql = `SELECT name, price FROM menus WHERE restaurant_id=${id}`;
+    const [menuInfo] = await db.execute(sql);
+
+    const token = req.headers.authorization;
+    let scrap = false;
+
+    // 스크랩 여부 조회
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userId = decoded.id;
+
+      // 스크랩 여부 확인 후 스크랩 시 값 true로 조정
+      sql = `SELECT EXISTS (SELECT 1 FROM scraps WHERE user_id=${userId} AND restaurant_id=${id}) AS scraped`;
+      [result] = await db.execute(sql);
+      if (result[0].scraped) scrap = true;
+    }
+
+    // 전달 양식
+    const detail = {
+      name,
+      category,
+      menu: menuInfo,
+      address,
+      hours,
+      phone,
+      isScraped: scrap,
+    };
+    res.status(200).json(detail);
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "찾을 수 없습니다.",
+    });
+  }
+};
+
+// 메뉴 스크랩여부

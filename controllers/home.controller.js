c412a65590d0beeff7332f1a76756e1105d4b0fd
Dong Hyun <decidere@naver.com>
2024-11-21 16:54:15 +0900

    [Feat]: 홈화면 컨트롤러 예외처리 추가

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,10 +1,16 @@
 const db = require("../utils/db");
 
 exports.getAllMarkers = async (req, res) => {
-  const sql = `SELECT id,name,category, 
+  try {
+    const sql = `SELECT id,name,category, 
   JSON_OBJECT('latitude', latitude, 'longitude', longitude) AS location 
   from restaurants`;
 
-  const [result] = await db.execute(sql);
-  res.status(200).json(result);
+    const [result] = await db.execute(sql);
+    res.status(200).json(result);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "서버 오류가 발생했습니다." });
+  }
 };

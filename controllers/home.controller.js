7b02e599e299d93b9ae7d1762b4732e55a3ce84e
Dong Hyun <decidere@naver.com>
2024-11-21 17:05:36 +0900

    [Feat]: 라벨별 홈화면 컨트롤러 구현

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -14,3 +14,19 @@ exports.getAllMarkers = async (req, res) => {
       .json({ success: false, message: "서버 오류가 발생했습니다." });
   }
 };
+
+exports.getLabelMarkers = async (req, res) => {
+  try {
+    const { label } = req.params;
+    const sql = `SELECT id,name,category, 
+  JSON_OBJECT('latitude', latitude, 'longitude', longitude) AS location 
+  from restaurants WHERE category=${label}`;
+
+    const [result] = await db.execute(sql);
+    res.status(200).json(result);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "서버 오류가 발생했습니다." });
+  }
+};

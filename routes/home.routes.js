f4fae162d241bd7a07c617056aa6d22dec93905e
Dong Hyun <decidere@naver.com>
2024-11-21 16:51:40 +0900

    [Feat]: app.js 라우터 분리 (home.routes)

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const getAllMarkers = require("../controllers/user.controller");
+const { getAllMarkers } = require("../controllers/home.controller");
 
 router.get("/", getAllMarkers);
 

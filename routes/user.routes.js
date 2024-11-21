a8de56900f89768efb129a7580c662fd3aad5d1b
Ss0Mae <ssomae@protonmail.ch>
2024-11-21 13:39:44 +0900

    [Feat]: 회원 정보 조회 라우팅

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/user.controller');
+const { registerUser, loginUser, getUserInfo } = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 // 회원가입
@@ -8,4 +9,6 @@ router.post('/register', registerUser);
 // 로그인
 router.post('/login', loginUser);
 
+// 회원 정보 조회
+router.get('/', authMiddleware, getUserInfo);
 module.exports = router;

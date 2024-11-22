802ab318ebdb758f4090f827735d3b0de9df15eb
Dong Hyun <decidere@naver.com>
2024-11-22 21:41:08 +0900

    [Feat]: 스크랩 목록 조회 라우팅 및 app.js 라우터 분리 (scraps.routes)

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ const userRoutes = require("./routes/user.routes");
 const homeRoutes = require("./routes/home.routes");
 const restaurantRoutes = require("./routes/restaurant.routes");
 const searchRoutes = require("./routes/search.routes");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swagger'); // Swagger 설정 파일
-const cors = require('cors');
+const scrapRouter = require("./routes/scrap.routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger"); // Swagger 설정 파일
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
 // Swagger UI 라우트 추가
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-console.log('Swagger UI available at http://localhost:7777/api-docs');
+console.log("Swagger UI available at http://localhost:7777/api-docs");
 
 // CORS 설정
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
-    allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
+  allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
 };
 app.use(cors(corsOptions));
 
@@ -31,5 +32,6 @@ app.use("/user", userRoutes);
 app.use("/home", homeRoutes);
 app.use("/restaurant", restaurantRoutes);
 app.use("/search", searchRoutes);
+app.use("/scrap", scrapRouter);
 
 module.exports = app;
--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
+const { getScraps } = require("../controllers/scrap.controller");
+
+router.get("/", authMiddleware, getScraps);
 
 module.exports = router;

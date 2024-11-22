d1c66be5d1c6aceb4fe61d01a3749cad24fc444a
Dong Hyun <decidere@naver.com>
2024-11-22 14:39:07 +0900

    [Fix]: 라벨별 홈화면 컨트롤러 SQL문 수정

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -25,9 +25,14 @@ exports.getLabelMarkers = async (req, res) => {
   try {
     const { label } = req.query;
     // id, 이름, 카테고리(라벨), 위치{위도: , 경도:}
-    const sql = `SELECT id,name,category, 
-  JSON_OBJECT('latitude', latitude, 'longitude', longitude) AS location 
-  from restaurants WHERE category="${label}"`;
+    const sql = `SELECT r.id, r.name, JSON_ARRAYAGG(l.name) AS labels, 
+  JSON_OBJECT('latitude', r.latitude, 'longitude', r.longitude) AS location
+  FROM restaurants r
+  JOIN restaurant_labels rl ON r.id = rl.restaurant_id
+  JOIN labels l ON rl.label_id = l.id
+  WHERE  l.name = "${label}"
+  GROUP BY r.id;
+`;
 
     const [result] = await db.execute(sql);
     res.status(200).json(result);

8b4230fa977ec5fbe0d1dc5cec228e1fc8ab41db
Dong Hyun <decidere@naver.com>
2024-11-22 21:52:27 +0900

    [Feat]: 스크랩 해제 라우팅

--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
-const { getScraps } = require("../controllers/scrap.controller");
+const { getScraps, deleteScap } = require("../controllers/scrap.controller");
 
 // 스크랩 목록 조회
 router.get("/", authMiddleware, getScraps);
+router.post("/", authMiddleware, deleteScap);
 
 module.exports = router;

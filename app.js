2208e95cc48a772e1ad40d917c5bc83dc049abc0
Ss0Mae <ssomae@protonmail.ch>
2024-11-22 12:35:24 +0900

    [Feat]: CORS 설정

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const userRoutes = require("./routes/user.routes");
 const homeRoutes = require("./routes/home.routes");
 const restaurantRoutes = require("./routes/restaurant.routes");
 const searchRoutes = require("./routes/search.routes");
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
+
+// CORS 설정
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+    allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use("/user", userRoutes);

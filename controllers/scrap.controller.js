7f6218401569421acc025c21d2a780b06e7a52d4
Dong Hyun <decidere@naver.com>
2024-11-22 22:19:23 +0900

    [Feat]: 스크랩 목록 조회 무한 스크롤 로직 구현

--- controllers/scrap.controller.js
+++ controllers/scrap.controller.js
@@ -3,32 +3,46 @@ const db = require("../utils/db");
 // 스크랩 목록 조회 컨트롤러
 exports.getScraps = async (req, res) => {
   try {
-    // 레스토랑아이디, 이름, 라벨
     const userId = req.user.id;
+    let { cursor = 0, limit = 5 } = req.query;
+    cursor = +cursor;
+    limit = +limit;
 
-    // 레스토랑 아이디, 이름 조회
-    let sql = `SELECT r.id, r.name FROM restaurants r JOIN scraps s
-  ON r.id = s.restaurant_id WHERE s.user_id = ${userId}`;
-    const [restaurants] = await db.execute(sql);
+    // 리뷰 아이디, 레스토랑 아이디, 이름 조회
+    let sql = `SELECT s.id, r.id AS restaurantId, r.name 
+           FROM restaurants r 
+           JOIN scraps s ON r.id = s.restaurant_id 
+           WHERE s.user_id = ?
+           AND s.id > ?  -- 커서로 레스토랑을 제한
+           ORDER BY r.id ASC  -- 순차적으로 조회
+           LIMIT ?`;
+    const params = [userId, cursor, limit];
+    const [restaurants] = await db.query(sql, params);
 
     // 레스토랑 아이디로 라벨명 조회할 SQL문
     let labelSql = `SELECT l.name FROM restaurant_labels rl
   JOIN labels l ON rl.label_id = l.id WHERE rl.restaurant_id = ?;`;
 
-    // 출력값을 담을 배열
+    // 출력할 데이터들을 담을 배열
     const restaurantWithLabels = [];
 
-    // restaurants(스크랩 목록)를 순회하며 각 요소마다 SQL문을 실행해 라벨명을 조회
+    // restaurants(스크랩 목록)을 순회하며 각 요소마다 SQL문을 실행해 라벨명을 조회
     for (let restaurant of restaurants) {
-      const [labels] = await db.execute(labelSql, [restaurant.id]);
+      const [labels] = await db.execute(labelSql, [restaurant.restaurantId]);
       // 라벨명 배열 형식으로 저장
       restaurant.labels = [];
       labels.forEach((label) => restaurant.labels.push(label.name));
       restaurantWithLabels.push(restaurant);
     }
+
+    // 다음 요청에 사용할 커서
+    const lastCursor = restaurantWithLabels[restaurantWithLabels.length - 1].id;
+    restaurantWithLabels.push({ lastCursor });
+
     res.status(200).json(restaurantWithLabels);
   } catch (err) {
-    res.status(400).json({ succes: false, message: "잘못된 요청입니다." });
+    res.json(err);
+    // res.status(400).json({ succes: false, message: "잘못된 요청입니다." });
   }
 };
 

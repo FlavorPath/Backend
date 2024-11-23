cee90582709b7d1d663061620b5ddf7924fa771f
Dong Hyun <decidere@naver.com>
2024-11-23 17:28:47 +0900

    [Feat]: 리뷰 수정 라우팅

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {
   getAllReviews,
   getReview,
+  changeReview,
 } = require("../controllers/review.controller");
 
 // 유저가 작성한 모든 리뷰 불러오기
@@ -11,5 +12,8 @@ router.get("/", authMiddleware, getAllReviews);
 
 // 기존 리뷰 불러오기
 router.get("/:id", authMiddleware, getReview);
+
+// 리뷰 수정하기
+router.put("/:id", authMiddleware, changeReview);
 
 module.exports = router;

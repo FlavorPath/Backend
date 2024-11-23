6e0525cf77779fe504397ce4d1f40f957987052a
Dong Hyun <decidere@naver.com>
2024-11-23 17:36:58 +0900

    [Feat]: 리뷰 삭제 라우팅

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -5,6 +5,7 @@ const {
   getAllReviews,
   getReview,
   changeReview,
+  deleteReview,
 } = require("../controllers/review.controller");
 
 // 유저가 작성한 모든 리뷰 불러오기
@@ -15,5 +16,8 @@ router.get("/:id", authMiddleware, getReview);
 
 // 리뷰 수정하기
 router.put("/:id", authMiddleware, changeReview);
+
+// 리뷰 삭제하기
+router.delete("/:id", authMiddleware, deleteReview);
 
 module.exports = router;

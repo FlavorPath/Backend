00d3227f994bbde005a84ede2d7990c6a01ddc4e
Dong Hyun <decidere@naver.com>
2024-11-23 16:58:38 +0900

    [Feat]: 기존 리뷰 불러오기 라우팅

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
-const { getAllReviews } = require("../controllers/review.controller");
+const {
+  getAllReviews,
+  getReview,
+} = require("../controllers/review.controller");
 
 // 유저가 작성한 모든 리뷰 불러오기
 router.get("/", authMiddleware, getAllReviews);
+
+// 기존 리뷰 불러오기
+router.get("/:id", authMiddleware, getReview);
 
 module.exports = router;

1ab3176a33dcc695ca585ff2f0a9d30c7c0aee7c
Ss0Mae <ssomae@protonmail.ch>
2024-11-23 22:24:42 +0900

    [Comment]: 스크랩 목록 조회에 대한 Swagger 주석 수정

--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -33,27 +33,46 @@ const { getScraps, deleteScap } = require("../controllers/scrap.controller");
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     example: 3
- *                   restaurantId:
- *                     type: integer
- *                     example: 1
- *                   name:
- *                     type: string
- *                     example: "청진옥"
- *                   labels:
- *                     type: array
- *                     items:
- *                       type: string
- *                     example: ["한식", "국밥"]
- *                   lastCursor:
- *                     type: integer
- *                     example: 10
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 3
+ *                       restaurantId:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: "청진옥"
+ *                       labels:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                         example: ["한식", "국밥"]
+ *                 cursor:
+ *                   type: integer
+ *                   example: 10
+ *       404:
+ *         description: 스크랩된 식당을 찾을 수 없음
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "스크랩된 식당을 찾을 수 없습니다."
  *       401:
  *         description: 인증 실패 (JWT가 필요함)
  *         content:
@@ -81,6 +100,7 @@ const { getScraps, deleteScap } = require("../controllers/scrap.controller");
  *                   type: string
  *                   example: "서버 오류가 발생했습니다."
  */
+
 // 스크랩 목록 조회
 router.get("/", authMiddleware, getScraps);
 

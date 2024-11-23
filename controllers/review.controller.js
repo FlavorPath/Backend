66ef02fe84c1ef23f1cf027f6a619546ec9521d2
Dong Hyun <decidere@naver.com>
2024-11-23 16:49:11 +0900

    [Feat]: 리뷰 전체 조회 컨트롤러 예외 처리 및 응답데이터 디테일 추가

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,10 +1,16 @@
 const db = require("../utils/db");
 
 exports.getAllReviews = async (req, res) => {
-  const userId = req.user.id;
+  try {
+    const userId = req.user.id;
 
-  const sql = `SELECT id, content, created_at FROM reviews WHERE user_id = ${userId}`;
-  const [reviews] = await db.execute(sql);
+    const sql = `SELECT id, restaurant_id, content, created_at FROM reviews WHERE user_id = ${userId}`;
+    const [reviews] = await db.execute(sql);
 
-  res.json(reviews);
+    res.status(200).json(reviews);
+  } catch (err) {
+    res
+      .status(404)
+      .json({ success: false, message: "작성된 리뷰가 없습니다." });
+  }
 };

12052bd719c8108a7e3ffba6ed06da68aa7b1f87
Dong Hyun <decidere@naver.com>
2024-11-23 16:58:21 +0900

    [Feat]: 기존 리뷰 불러오기 컨트롤러 구현

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -14,3 +14,18 @@ exports.getAllReviews = async (req, res) => {
       .json({ success: false, message: "작성된 리뷰가 없습니다." });
   }
 };
+
+// 기존 리뷰 불러오기 (리뷰 수정 페이지로 이동 시)
+exports.getReview = async (req, res) => {
+  const reviewId = req.params.id;
+
+  const sql = `SELECT id, restaurant_id, content, created_at FROM reviews WHERE id = ${reviewId}`;
+  const [review] = await db.execute(sql);
+
+  if (!review.length) {
+    return res
+      .status(404)
+      .json({ success: false, message: "리뷰를 찾을 수 없습니다" });
+  }
+  res.status(200).json(review);
+};

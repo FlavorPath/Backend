7085da75f719a953d92dfacebe7692fc4a01ff04
Dong Hyun <decidere@naver.com>
2024-11-26 15:46:10 +0900

    [Feat]: 작성한 리뷰 조회 응답 데이터에 라벨 데이터 추가

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -7,8 +7,15 @@ exports.getAllReviews = async (req, res) => {
     const limit = 10;
     cursor = +cursor;
 
-    const sql = `SELECT id, restaurant_id, content, created_at FROM reviews WHERE user_id = ?
-    AND id > ? ORDER BY id ASC LIMIT ?`;
+    const sql = `WITH RankedLabels AS (
+  SELECT rl.restaurant_id, l.name,
+  ROW_NUMBER() OVER (PARTITION BY rl.restaurant_id ORDER BY rl.label_id ASC) AS row_num
+  FROM restaurant_labels rl JOIN labels l ON rl.label_id = l.id)
+  SELECT r.id AS id, r.restaurant_id, r.content, r.created_at, rl.name AS label
+  FROM reviews r LEFT JOIN RankedLabels rl ON r.restaurant_id = rl.restaurant_id AND rl.row_num = 1
+  WHERE r.user_id = ? AND r.id > ?
+  ORDER BY r.id ASC LIMIT ?; `;
+
     const params = [userId, cursor, limit];
     const [reviews] = await db.query(sql, params);
 

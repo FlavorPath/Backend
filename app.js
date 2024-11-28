0fb225b8378f30f0aa88949b40d1f706ce195304
Ss0Mae <ssomae@protonmail.ch>
2024-11-28 17:10:31 +0900

    [Fix]: dist Test

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
 const userRoutes = require("./routes/user.routes");
 const homeRoutes = require("./routes/home.routes");
 const restaurantRoutes = require("./routes/restaurant.routes");
@@ -8,23 +11,20 @@ const searchRoutes = require("./routes/search.routes");
 const scrapRouter = require("./routes/scrap.routes");
 const reviewRoutes = require("./routes/review.routes");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocs = require("./swagger"); // Swagger 설정 파일
-const cors = require("cors");
-const path = require("path");
+const swaggerDocs = require("./swagger");
+
 dotenv.config();
 
 const app = express();
 
-// Swagger UI 라우트 추가
+// Swagger UI 설정
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 console.log("Swagger UI available at http://localhost:1234/api-docs");
 
 // CORS 설정
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ["http://localhost:5173", "http://localhost:1234", 'http://43.202.172.0:1234/'];
-    console.log("Origin: ", origin);
+    const allowedOrigins = ["http://localhost:5173", "http://43.202.172.0:1234"];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -38,21 +38,21 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
-// 1. dist 폴더를 정적 파일로 제공
-app.use(express.static(path.join(__dirname, "dist")));
-
-// 2. React 라우터를 위해 모든 요청을 index.html로 리디렉션
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
-});
-
+// Body parser 설정
 app.use(bodyParser.json());
+
+// API 라우트
 app.use("/api/user", userRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/restaurant", restaurantRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/scrap", scrapRouter);
 app.use("/api/user/review", reviewRoutes);
+
+// React 정적 파일 서빙
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 module.exports = app;
